19280da5404cfe8c62a4ca7a8344dddca1d83d05
Kenneth Crawford <kcrawford@triumphpay.com>
2023-12-07 23:06:46 -0800

    hmmmm

--- AoC2023/Day08.fs
+++ AoC2023/Day08.fs
@@ -27,7 +27,7 @@ let parseDirections line =
     
 let parseMap lines =
     let parseNode line =
-        let parts = line |> StringExtras.matches (Regex("[A-Z]{3}", RegexOptions.Compiled)) |> Seq.toArray
+        let parts = line |> StringExtras.matches (Regex("[\dA-Z]{3}", RegexOptions.Compiled)) |> Seq.toArray
         let name = parts[0]
         let left = parts[1]
         let right = parts[2]
@@ -35,50 +35,78 @@ let parseMap lines =
     
     splitInputByNewLines lines
     |> Seq.map parseNode
-    |> tapValues
     |> Map.ofSeq
     
 
 let parse = splitInputByDoubleNewLines >> SeqExtras.pluckFirst2ToTuple >> TupleExtras.mapFst parseDirections >> TupleExtras.mapSnd parseMap
 
-let findPath directions map =
-    let rec loop node directions' path =
-        match node.name with
-        | "ZZZ" -> path
-        | _ -> match directions' with
-               | [] -> loop node directions path
+let recLimit = 1000000
+
+let findPath start anyEnd directions map =
+    let rec loop node directions' steps =
+        match steps = recLimit || Set.contains node.name anyEnd with
+        | true -> steps
+        | false ->
+               match directions' with
+               | [] -> loop node directions steps
                | Left::rest ->
                    let nextNode = (Map.find node.left map)
-                   loop nextNode rest (nextNode::path)
+                   loop nextNode rest (steps + 1)
                | Right::rest ->
                    let nextNode = (Map.find node.right map)
-                   loop nextNode rest (nextNode::path)
-    loop (Map.find "AAA" map) directions []
-
-let breadthFirstSearch directions map =
-    let rec loop queue visited remainingDirections =
-        match queue with
-        | [] -> failwith "Asdf"
-        | (node, path)::rest ->
-            if node = "ZZZ" then path
-            else
-                let node = Map.find node map
-                let nextNode,nextDirections = 
-                    match directions with
-                    | Left::rest -> node.left, rest
-                    | Right::rest -> node.right, rest
-                    | _ -> failwith "Invalid directions"
-                    |> TupleExtras.mapFst (flip Map.find map)
-                let visited = Set.add node visited
-                let queue = 
-                    if Set.contains nextNode visited then queue
-                    else (nextNode.name, path + "->" + nextNode.name)::queue
-                loop rest visited nextDirections
-    loop [("AAA", "")] Set.empty directions
+                   loop nextNode rest (steps + 1)
+    loop (Map.find start map) directions 0
     
-let solve (directions,map) = findPath directions map |> Seq.length
+let solve (directions,map) = findPath "AAA" (Set.ofList ["ZZZ"]) directions map
+
+//This takes too long but is interesting.
+[<TailCall>]
+let rec loop map finalNodes allDirections currentNodes directions' steps =
+    if steps % 1000000 = 0 then printfn $"step %d{steps}"
+    match (Set.ofSeq currentNodes) = finalNodes with
+    | true -> steps
+    | false ->
+        let findNextNodes selectNode =
+            currentNodes
+            |> List.map (fun n -> (Map.find (selectNode n) map))
+            
+        let nextNodes,nextDirections,nextSteps =
+            match directions' with
+            | [] ->
+                currentNodes,allDirections,steps
+            | Left::rest -> (findNextNodes _.left),rest,(steps + 1)
+            | Right::rest -> (findNextNodes _.right),rest,(steps + 1)
+        loop map finalNodes allDirections nextNodes nextDirections nextSteps
+       
+
+let answer2 (directions,map) =
+    let startingNodes = map |> Map.values |> Seq.filter (_.name.EndsWith("A")) |> Seq.toList
+    let finalNodes = map |> Map.values |> Seq.filter (_.name.EndsWith("Z")) |> Seq.map _.name |> Set.ofSeq 
+            
+    // loop map finalNodes directions startingNodes directions 0
+    
+    //this finds the shortest path for each node, and then multiplies them together
+    //so so wrong because the paths dont continually loop around
+    // startingNodes
+    // |> Seq.map (fun n -> findPath n.name finalNodes directions map)
+    // |> Seq.map Seq.length
+    // |> Seq.map uint64
+    // |> tapValues
+    // |> List.ofSeq
+    // |> Seq.reduce (*)
+    // |> ignore
+    
+    startingNodes
+    |> Seq.map (fun n ->
+        finalNodes
+        |> Seq.map (fun finalNode -> findPath n.name (Set.ofList [finalNode]) directions map)
+        |> Seq.filter ((>) recLimit)
+        |> List.ofSeq)
+    |> tapValues
+    |> Seq.length
+
 
 let solve1 = parse >> solve
-let solve2 = id
+let solve2 = parse >> answer2
 
-let printAnswer = printAnswersWithSameInputs1 solve1 solve2 example1 p1
+let printAnswer = printAnswers2 solve1 example1 p1 solve2 example2 p2
--- AoC2023/Inputs.fs
+++ AoC2023/Inputs.fs
@@ -3512,8 +3512,17 @@ BVM = (KHF, FTD)
 XPK = (SQN, MLN)
 GJN = (MLR, XHT)"
     
-    let example2 = "08"
-    let p2 = ""
+    let example2 = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"
+    let p2 = p1
     
 module DayXX =
     let example1 = ""
