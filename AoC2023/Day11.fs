a0b2d0e63c82c039b9a679a899545891e811ef38
Kenneth Crawford <kcrawford@triumphpay.com>
2023-12-19 21:20:18 -0800

    day 11 2

--- AoC2023/Day11.fs
+++ AoC2023/Day11.fs
@@ -34,39 +34,43 @@ let parse input =
     makeUniverse galaxies
     
 
-let expandUniverse universe =
+let expandUniverse dist universe =
+    // this is how many additional rows/cols to _add_ for the expansion.
+    // If we are adding 1 empty row represents 10 expanded rows, `dist` would be 10, but we only need
+    // to add 9 rows in this logic to get the full 10 since the original row remains.
+    let expansionDist = dist - 1
     let knownRows = universe.galaxies |> List.map _.row |> Set.ofList
     let knownCols = universe.galaxies |> List.map _.col |> Set.ofList    
     let emptyRows = [1..universe.maxRow] |> List.filter (flip Set.contains knownRows >> not)
     let emptyCols = [1..universe.maxCol] |> List.filter (flip Set.contains knownCols >> not)
     
-    printfn $"knownRows: %A{knownRows}"
-    printfn $"knownCols: %A{knownCols}"
-    
-    printfn $"emptyRows: %A{emptyRows}"
-    printfn $"emptyCols: %A{emptyCols}"
+    // printfn $"knownRows: %A{knownRows}"
+    // printfn $"knownCols: %A{knownCols}"
+    //
+    // printfn $"emptyRows: %A{emptyRows}"
+    // printfn $"emptyCols: %A{emptyCols}"
     
     let galaxies =
         universe.galaxies
         |> List.map (fun galaxy ->
-            let rowsToAdd = emptyRows |> List.sumBy (fun emptyRow -> if emptyRow < galaxy.row then 1 else 0)
-            let colsToAdd = emptyCols |> List.sumBy (fun emptyCol -> if emptyCol < galaxy.col then 1 else 0)
+            let rowsToAdd = expansionDist * (emptyRows |> List.sumBy (fun emptyRow -> if emptyRow < galaxy.row then 1 else 0))
+            let colsToAdd = expansionDist * (emptyCols |> List.sumBy (fun emptyCol -> if emptyCol < galaxy.col then 1 else 0))
             { row = galaxy.row + rowsToAdd; col = galaxy.col + colsToAdd }
         )
     
     { galaxies = galaxies
-      maxRow = universe.maxRow + emptyRows.Length
-      maxCol = universe.maxCol + emptyCols.Length; 
+      maxRow = universe.maxRow + (emptyRows.Length * expansionDist)
+      maxCol = universe.maxCol + (emptyCols.Length * expansionDist); 
     }
 
 
 let measureDistance g1 g2 = manhattanDistance (g1.row, g1.col) (g2.row, g2.col)
 
-let testDistances =
-    printfn $"%i{(measureDistance { row = 7; col = 2 } { row = 12; col = 6 })}"
-    printfn $"%i{(measureDistance { row = 1; col = 5 } { row = 8; col = 13 })}"
-    printfn $"%i{(measureDistance { row = 3; col = 1 } { row = 8; col = 13 })}"
-    printfn $"%i{(measureDistance { row = 9; col = 1 } { row = 9; col = 6 })}"
+// let testDistances =
+//     printfn $"%i{(measureDistance { row = 7; col = 2 } { row = 12; col = 6 })}"
+//     printfn $"%i{(measureDistance { row = 1; col = 5 } { row = 8; col = 13 })}"
+//     printfn $"%i{(measureDistance { row = 3; col = 1 } { row = 8; col = 13 })}"
+//     printfn $"%i{(measureDistance { row = 9; col = 1 } { row = 9; col = 6 })}"
     
 
 let allCombos universe =
@@ -82,11 +86,9 @@ let allCombos universe =
 let allDistances universe =    
     universe |> allCombos |> List.sumBy (fun (g1, g2) -> measureDistance g1 g2)
 
-let solve1 = parse >> expandUniverse >> printUniverse >> allDistances
+let solve1 = parse >> expandUniverse 2 >> printUniverse >> allDistances
 
-//let solve1 _ = testDistances
-
-let solve2 = id
+let solve2 = parse >> expandUniverse 1_000_000 >> allDistances
     
 
-let printAnswer = printAnswersWithSameInputs1 solve1 solve2 example1 p1
+let printAnswer = printAnswersWithSameInputs2 solve1 solve2 example1 p1
