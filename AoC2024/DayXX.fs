e0e15541a884fb52a5e13cca0d541f017d96e888
Kenneth Crawford <kcrawford@triumphpay.com>
2024-11-25 15:38:42 -0800

    setup 2024

---
+++ AoC2024/DayXX.fs
@@ -0,0 +1,37 @@
+﻿module Day01
+
+open System
+open Utils
+open AoC2023.Inputs.Day01
+
+let p1Cases = [1..9] |> List.map (fun x -> (x.ToString(), x))
+let p2Cases =
+    [ "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" ]
+    |> List.mapi (fun i x -> (x.ToString(), (i + 1)))
+    |> List.append p1Cases
+    
+let findMatch findIndex matchPicker cases str =
+    cases
+    |> List.map (fun (matchText, value) -> (findIndex matchText str, value))
+    |> List.filter (fst >> isPositive)
+    |> matchPicker fst
+    |> snd
+   
+let firstMatch = findMatch indexOf List.minBy
+let lastMatch = findMatch lastIndexOf List.maxBy
+
+let calibrateLine cases str = (firstMatch cases str * 10) + (lastMatch cases str)
+let calibrateLines calibrateLine = splitInputByNewLines >> Seq.sumBy calibrateLine
+
+let solve1 = calibrateLines <| calibrateLine p1Cases
+let solve2 = calibrateLines <| calibrateLine p2Cases
+
+let print1 =
+    Console.WriteLine(solve1 day1p1Example)
+    Console.WriteLine(solve1 day1p1)
+    ()
+   
+let print2 =
+    Console.WriteLine(solve2 day1p2example)
+    Console.WriteLine(solve2 day1p1)
+    ()
---
+++ AoC2024/Program.fs
@@ -0,0 +1,22 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open Day01
+print1
+print2
+
+//open Day02
+//open Day03
+//open Day04
+// open Day05
+// open Day06
+// open Day07
+// open Day08
+// open Day09
+// open Day10
+// open Day11
+// open Day12
+// open Day13
+// open Day14
+// open Day15
+// open Day16
+// open Day18
+
